ddfaa28209d16248fea6d789136e4d4e383ad081
Jakub Kutka <jacobkutka@gmail.com>
2021-02-11 13:55:34 +0100

    Static Method of a Class

---
+++ src/sk/itsovy/kutka/Addition.java
@@ -0,0 +1,8 @@
+package sk.itsovy.kutka;
+
+public class Addition {
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+}
--- src/sk/itsovy/kutka/Main.java
+++ src/sk/itsovy/kutka/Main.java
@@ -1,10 +1,16 @@
 package sk.itsovy.kutka;
 
+import java.util.function.BiFunction;
+
 public class Main {
 
     public static void main(String[] args) {
 	Car car = new Car();
 	CarInterface ref = car::fuelType;
 	ref.printFuelType("Gasoline");
+
+	BiFunction<Integer, Integer, Integer> product = Addition::add;
+	int pr = product.apply(5, 5);
+	System.out.println("Product of given number is: " + pr);
     }
 }
